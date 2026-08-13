1fd76aa8d139450b08c855d4fb3e96840ab71a2d
zun3017 <hodung300107@gmail.com>
2026-08-13 15:05:35 +0700

    Fix row 0 transparent frame issue in player_drown.png sprite sheet

--- src/Entities/CPEOPLE.cpp
+++ src/Entities/CPEOPLE.cpp
@@ -70,7 +70,8 @@ void CPEOPLE::update(float dt) {
             auto tex = m_sprite.getTexture();
             int frameW = static_cast<int>(tex->getSize().x) / 4;
             int frameH = static_cast<int>(tex->getSize().y) / 4;
-            m_sprite.setTextureRect(sf::IntRect(currentFrame * frameW, m_animRow * frameH, frameW, frameH));
+            int rowToDraw = (m_animRow == 0) ? 1 : m_animRow;
+            m_sprite.setTextureRect(sf::IntRect(currentFrame * frameW, rowToDraw * frameH, frameW, frameH));
         }
         return;
     }
@@ -220,7 +221,8 @@ void CPEOPLE::die(DeathType type, const sf::Texture* deathTexture) {
                 auto texSize = tex->getSize();
                 int frameW = static_cast<int>(texSize.x) / 4;
                 int frameH = static_cast<int>(texSize.y) / 4;
-                m_sprite.setTextureRect(sf::IntRect(0, m_animRow * frameH, frameW, frameH));
+                int rowToDraw = (m_animRow == 0) ? 1 : m_animRow;
+                m_sprite.setTextureRect(sf::IntRect(0, rowToDraw * frameH, frameW, frameH));
                 m_sprite.setScale(PLAYER_SIZE / static_cast<float>(frameW), PLAYER_SIZE / static_cast<float>(frameH));
                 m_sprite.setOrigin(0.f, 0.f);
                 m_isDrowned = false;
